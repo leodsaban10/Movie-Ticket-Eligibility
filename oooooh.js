abd7afe4fef6a68d43378840a04ed24e2de9036c
Leo Saban <leodsaban@gmail.com>
2025-09-26 08:13:38 -0600

    added pokemon fetch api js and html

--- oooooh.js
+++ oooooh.js
@@ -1,3 +1,19 @@
-// this is what you do to get something.
-// you call the function, and it returns something.
-const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+
+async function fetchPokemon() {
+    try {
+       const response = await fetch("https://pokeapi.co/api/v2/pokemon")
+       const pokemon = await response.json()
+        return pokemon.results;
+    } catch(error) {
+        console.log(error);
+    }
+}
+fetchPokemon().then(pokemon => {
+    const list = document.getElementById('pokemon-list');
+    pokemon.forEach(p => {
+        const item = document.createElement('div');
+        item.textContent = p.name;
+        list.appendChild(item);
+    });
+});
+
