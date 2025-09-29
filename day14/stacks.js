c35b7ffa2ced22b523f989463beb31a11f2882b1
Leo Saban <leodsaban@gmail.com>
2025-09-29 08:49:59 -0600

    added day 14 stacks

---
+++ day14/stacks.js
@@ -0,0 +1,7 @@
+//stack properties
+// push(element) - adds an element to the top of the stack
+// pop() - removes and returns the top element of the stack
+// peek() - returns the top element of the stack without removing it
+// isEmpty() - checks if the stack is empty
+// size() - returns the number of elements in the stack
+// clear() - removes all elements from the stack
